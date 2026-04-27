fa45287a039f85337183ce14ba385ec2de443d0d
mhmdah <mhmdabouhamoud@gmail.com>
2026-04-27 16:45:57 +0200

    did some changes

--- person/urls.py
+++ person/urls.py
@@ -18,6 +18,6 @@ urlpatterns = [
     path('',include(router.urls)),
     path('transactions/profile/<str:profile_id>/', TransactionListCreateView.as_view(), name='transaction-list-create'),
     path('transactions/<int:pk>',TransactionRetrieveUpdateDestroyView.as_view(), name='transaction-detail'),
-    path('transactions/profile/<int:profile_id>/monthly/', MonthlyTransactionListView.as_view(), name='transaction-monthly'),
-    path('transactions/profile/<int:profile_id>/last-ten-days/', LastTenDaysTransactionListView.as_view(), name='transaction-ten-days'),
+    path('transactions/profile/<str:profile_id>/monthly/', MonthlyTransactionListView.as_view(), name='transaction-monthly'),
+    path('transactions/profile/<str:profile_id>/last-ten-days/', LastTenDaysTransactionListView.as_view(), name='transaction-ten-days'),
 ]
--- person/views.py
+++ person/views.py
@@ -10,7 +10,7 @@ class CategoryViewSet(viewsets.ModelViewSet):
     serializer_class = CategorySerializer
     permission_classes = [permissions.IsAuthenticated]
     def get_queryset(self):
-        return Category.objects.filter(Q(profile__user=self.request.user)| Q(profile = None))
+        return Category.objects.filter(Q(profile__user=self.request.user)| Q(profile__isnull = True))
     def perform_create(self, serializer):
         profile = Profile.objects.get(user=self.request.user)
         serializer.save(profile=profile)

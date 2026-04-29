e53d7ec26fa27843183c661d982273f9d6987204
mhmdah <mhmdabouhamoud@gmail.com>
2026-04-29 16:25:06 +0200

    remove pagination

--- person/urls.py
+++ person/urls.py
@@ -17,7 +17,7 @@ router.register(r'budgets', BudgetViewSet, basename='budget')
 urlpatterns = [
     path('',include(router.urls)),
     path('transactions/profile/<str:profile_id>/', TransactionListCreateView.as_view(), name='transaction-list-create'),
-    path('transactions/<int:pk>',TransactionRetrieveUpdateDestroyView.as_view(), name='transaction-detail'),
+    path('transactions/<int:id>/',TransactionRetrieveUpdateDestroyView.as_view(), name='transaction-detail'),
     path('transactions/profile/<str:profile_id>/monthly/', MonthlyTransactionListView.as_view(), name='transaction-monthly'),
     path('transactions/profile/<str:profile_id>/last-ten-days/', LastTenDaysTransactionListView.as_view(), name='transaction-ten-days'),
 ]
